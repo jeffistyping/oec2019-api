99a041c65da2e405c8f582eaca46631ed23e99f3
Jeff <jeffrey.kl.hui@gmail.com>
2019-01-18 23:48:26 -0500

    make it better again lmao

--- app.py
+++ app.py
@@ -7,55 +7,52 @@ from passlib.hash import sha256_crypt
 import datetime
 import os
 
+
+# MediHelper API
+'''
+The Medihelper API allows users to report medical checkup data to the database 
+and allow healthcare professionals to quickly screeen issues to determine if a follow up
+is required.
+'''
+
+# Database Connection Setup
 user = os.environ.get('URI_USER')
 password = os.environ.get('URI_PASS')
-
 uri = 'mongodb://' + user + ':' + password + '@ds161104.mlab.com:61104/ryeoec2019'
-
 client = MongoClient(uri, connectTimeoutMS=30000)
 db = client.get_database("ryeoec2019")
 hospital = db.hospital
-# password = sha256_crypt.encrypt("jimmy")
-# gender="male"
-# pnumber="109238120"
-# symptoms="cough"
-# post_data = {
- 	#     		'name': password,
-#     		'gender': gender,
-#     		'pnumber': pnumber,
-#     		'password': password,
-#     		'symptoms': symptoms,
-#     		'date': datetime.datetime.utcnow()
-#     	}
-# result = hospital.insert_one(post_data)
-# print(hospital.find_one())
 
-
+# Application Startup
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/register', methods=['GET','POST'])
-def register():
+@app.route('/send', methods=['GET','POST'])
+def send():
 	form = RegisterForm(request.form)
 	if request.method == 'POST' and hospital.find_one({"name": form.name.data}) == None:
-		name = form.name.data
-		gender = form.gender.data
-		pnumber = form.phonenumber.data
-		password = sha256_crypt.encrypt(str(form.password.data))
-		symptoms = form.symptoms.data
+		name = request.form['name']
+		gender = request.form['gender']
+		pnumber = request.form['phonenumber']
+		symptoms = request.form['symptoms']
+		doctor = request.form['doctor']
+		apptdate = request.form['apptdate']
 		post_data = {
     		'name': name,
     		'gender': gender,
     		'pnumber': pnumber,
-    		'password': password,
     		'symptoms': symptoms,
+    		'doctor': doctor,
+    		'apptdate': apptdate,
     		'date': datetime.datetime.utcnow()
     	}
 		result = hospital.insert_one(post_data)
-		hopital.find_one()
 		return name
 	return "An account already exists"
 
+@app.route("/api")
+def api():
+	return "/send - POST client data <br/> /login - Doctors Login"
 
 # @app.route('/login', methods=['GET','POST'])
 # def login():
@@ -65,8 +62,8 @@ def register():
 # 		password = sha256_crypt.encrypt(str(form.password.data))
 # 		if hospital.find_one({"name": form.name.data}) != None:
 # 			try_login = hospital.find_one({"name": form.name.data})
-
-
+# 			if try_login['password'] == password:
+# 				session
 
 if __name__ == "__main__":
 	app.run()
