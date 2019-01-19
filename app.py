70cc0d18cdf685864ed4cdaa0df8ddce55552254
Jeff <jeffrey.kl.hui@gmail.com>
2019-01-18 21:48:56 -0500

    good venv

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for, session, logging, send_from_directory
 from flask_cors import CORS
 import pymongo
 from pymongo import MongoClient
 import json
-import datetime 
-import pprint
 from creds import *
+from passlib.hash import sha256_crypt
 
 user, password = creds()
 
@@ -17,6 +16,23 @@ hospital = db.hospital
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+	form = RegisterForm(request.form)
+	if request.method == 'POST':
+		name = form.name.data
+		email = form.email.data
+		username = form.username.data
+		password = sha256_crypt.encrypt(str(form.password.data))
+		# push to db
+		return True
+	return False
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	form = RegisterForm(reque)
+
 
 if __name__ == "__main__":
 	app.run()
